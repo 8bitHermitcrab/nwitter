84b2b7174075643facbd8cd9055f9cb85fa16d32
KIJ <kij@KIJui-MacBookAir.local>
2021-11-14 14:57:57 +0900

    1.0 Initialization

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
+import { authService } from "fbase";
 import { useState } from "react";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
 
 
     const onChange = (event) => {
@@ -16,9 +19,24 @@ const Auth = () => {
         }
     };
 
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
-    }
+        try {
+            let data
+            if (newAccount) {
+                // create newAccount
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                // log in
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
 
     return (
         <div>
@@ -39,8 +57,12 @@ const Auth = () => {
                     value={password}
                     onChange={onChange}
                     />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                {error}
             </form>
+            <span onClick={toggleAccount}>
+            {newAccount ? "Sign In" : "Create Account"}
+            </span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
