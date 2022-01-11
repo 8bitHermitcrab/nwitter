07f44164bc6c4dffae5cb987f2965296d2f479b8
8bithermitcrab <kij517@naver.com>
2022-01-12 02:02:31 +0900

    1.0 Initialization

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
 import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
@@ -8,8 +9,11 @@ const AppRouter = ({ isLoggedIn }) => {
         <Router> {isLoggedIn && <Navigation />}
             <Routes>
                 {isLoggedIn ? (
-                    <Route path="/" element={<Home />} />
-                    ) : (
+                    <>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/" element={<Profile />} />
+                    </>
+            ) : (
                     <Route path="/" element={<Auth />} />
                     )}
             </Routes>
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,28 +1,27 @@
-import { authService, firebaseIntance } from "fbase";
+import { authService} from "fbase";
 import { useState } from "react";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [newAccount, setNewAccount] = useState(true);
+    const [newAccount, setNewAccount] = useState(true); // eslint-disable-line no-unused-vars
     const [error, setError] = useState("");
-
 
     const onChange = (event) => {
         const {
-            target: { name, value },
+            target: { name, value},
         } = event;
         if (name === "email") {
             setEmail(value);
         } else if (name === "password") {
             setPassword(value);
-        }
+        } 
     };
 
     const onSubmit = async (event) => {
         event.preventDefault();
         try {
-            let data
+            let data;
             if (newAccount) {
                 // create newAccount
                 data = await authService.createUserWithEmailAndPassword(email, password);
@@ -36,52 +35,31 @@ const Auth = () => {
         }
     };
 
-    const toggleAccount = () => setNewAccount((prev) => !prev);
-
-    const onSocialClick = async (event) => {
-        const{
-            target: { name },
-        } = event;
-        let provider;
-        if (name === "google") {
-            provider = new firebaseIntance.auth.GoogleAuthProvider();
-        } else if (name === "github") {
-            provider = new firebaseIntance.auth.GithubAuthProvider();
-        }
-        const data = await authService.signInWithPopup(provider);
-        console.log(data);
-    };
-
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input 
-                    name="email"
+                <input
+                    name="email" 
                     type="email" 
                     placeholder="Email" 
                     required
                     value={email}
                     onChange={onChange}
                 />
-                <input 
+                <input
                     name="password"
                     type="password" 
                     placeholder="Password" 
                     required
                     value={password}
                     onChange={onChange}
-                    />
+                />
                 <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
                 {error}
             </form>
-            <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
             <div>
-                <button onClick={onSocialClick} name="google">
-                    Continue with Google
-                </button>
-                <button onClick={onSocialClick} name="github">
-                    Continue with Github
-                </button>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
             </div>
         </div>
     );
