65959e649e414ba95f94d608f3c3d8c97dc3438d
8bithermitcrab <kij517@naver.com>
2022-01-15 01:06:18 +0900

    1.0 Initialization

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,11 +3,19 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]); // eslint-disable-line no-unused-vars
 
     const getNweets = async () => {
         const dbNweets = await dbService.collection("nweets").get();
-        console.log(dbNweets);
+        dbNweets.forEach((document) => {
+            const nweetObject = { ...document.data(), id: document.id };
+            setNweet((prev) => [nweetObject, ...prev])
+        });
     };
+
+    useEffect(() => {
+        getNweets();
+    }, []);
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -27,16 +35,25 @@ const Home = () => {
     };
 
     return (
-        <form onSubmit={onSubmit}>
-            <input
-                value={nweet}
-                onChange={onChange}
-                type="text"
-                placeholder="What's on your mind?"
-                maxLength={120}
-            />
-            <input type="submit" value="Nweet" />
-        </form>
+        <>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={nweet}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="What's on your mind?"
+                    maxLength={120}
+                />
+                <input type="submit" value="Nweet" />
+            </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.text}</h4>
+                    </div>
+                ))}
+            </div>
+        </>
     );
 };
 
