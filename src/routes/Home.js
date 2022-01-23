eb0d438fdd0cd4d56af06f7b6a695820de37b010
8bithermitcrab <kij517@naver.com>
2022-01-23 23:38:04 +0900

    1.0 Initialization

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,20 @@
 import { dbService } from "fbase";
 import { useEffect, useState } from "react";
+import Nweet from "components/Nweet";
 
-const Home = ( { useObj } ) => {
-    console.log(useObj);
+const Home = ( { userObj } ) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]); // eslint-disable-line no-unused-vars
 
-    const getNweets = async () => {
-        const dbNweets = await dbService.collection("nweets").get();
-        dbNweets.forEach((document) => {
-            const nweetObject = { ...document.data(), id: document.id };
-            setNweet((prev) => [nweetObject, ...prev])
-        });
-    };
 
     useEffect(() => {
-        getNweets();
+        dbService.collection("nweets").onSnapshot((snapshot) => {
+           const newArray = snapshot.docs.map((document) => ({
+            id: document.id,
+            ...document.data(),
+           }));
+           setNweets(newArray);
+        });
     }, []);
 
     const onSubmit = async (event) => {
@@ -50,9 +49,11 @@ const Home = ( { useObj } ) => {
             </form>
             <div>
                 {nweets.map((nweet) => (
-                    <div key={nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
+                    <Nweet 
+                        key={nweet.id} 
+                        nweetObj={nweet}
+                        isOwner={nweet.creatorId === userObj.uid}
+                    />
                 ))}
             </div>
         </>
